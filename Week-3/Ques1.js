e3a986064c9cfc72bd6d831ea4732cc86a1d8731
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-09-04 11:52:10 +0530

    first question done

---
+++ Week-3/Ques1.js
@@ -0,0 +1,40 @@
+//  Create a memoize function that remembers previous inputs and stores them in cache so that itwon’t have to compute the same inputs more than once. The function will take an unspecifiednumber of integer inputs and a reducer method
+
+// Memoization is an optimisation technique that is used to reduce the time-consuming calculations by saving the previous input value results in something called cache and returning the result from it .
+
+// Example
+
+const fib=(n)=>
+{
+ if(n<2)
+ {
+    return n;
+ }
+ return fib(n-1)+fib(n-2);
+}
+
+const memoize=(func)=>
+{
+    let cache={};
+    return function (...arg)
+    {
+       let n=arg[0];
+       if(n in cache)
+       {
+        return cache[n];
+       }
+       else{
+        let result=fib(n)
+        cache[n]=result;
+        return result;
+       }
+    }
+}
+console.time()
+const efficient=memoize(fib)
+console.log(efficient(40))
+console.timeEnd();
+
+console.time()
+console.log(efficient(40))
+console.timeEnd();
